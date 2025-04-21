aff3f6ad0b03a5467738701c1ad8348a577849bb
Renzzo Rocha <renzzorocha17@gmail.com>
2025-04-21 18:54:36 -0300

    feat: add funcao exibir tarefas

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ tarefas = []
 def adicionarTarefa(tarefa):
     novaTarefa = (tarefa, "pendente")
     tarefas.append(novaTarefa)
+
+def exibirTarefas():
+    for tarefa in tarefas:
+        print(f'{tarefa[0]} - Status: {tarefa[1]}')
