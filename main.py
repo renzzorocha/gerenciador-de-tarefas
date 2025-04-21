30130d87442b9d9a6ce811f1b438aaec976d6434
Renzzo Rocha <renzzorocha17@gmail.com>
2025-04-21 19:02:03 -0300

    feat: add funcao de concluir tarefa (need refact)

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ def adicionarTarefa(tarefa):
 def exibirTarefas():
     for tarefa in tarefas:
         print(f'{tarefa[0]} - Status: {tarefa[1]}')
+
+def concluirTarefa(tarefa):
+    global tarefas
+    novaLista = []
+    for t in tarefas:
+        if(t[0] == tarefa):
+            novaLista.append((tarefa, 'concluida'))
+        else:
+            novaLista.append(t)
+        tarefas = novaLista
