e981e9ef440ade93706722e77c5257cb88210435
Renzzo Rocha <renzzorocha17@gmail.com>
2025-04-21 19:17:31 -0300

    refact: refatorando as funcs

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 tarefas = []
 
 def adicionarTarefa(tarefa):
-    novaTarefa = (tarefa, "pendente")
+    novaTarefa = (tarefa, 'pendente')
     tarefas.append(novaTarefa)
 
 def exibirTarefas():
-    for tarefa in tarefas:
-        print(f'{tarefa[0]} - Status: {tarefa[1]}')
+    for t in tarefas:
+        print(f'{t[0]} - Status: {t[1]}')
 
 def concluirTarefa(tarefa):
     global tarefas
     novaLista = []
     for t in tarefas:
-        if(t[0] == tarefa):
+        if t[0] == tarefa:
             novaLista.append((tarefa, 'concluida'))
         else:
             novaLista.append(t)
-        tarefas = novaLista
+    tarefas = novaLista
+
+adicionarTarefa('teste')
+exibirTarefas()
+concluirTarefa('teste')
+exibirTarefas()
